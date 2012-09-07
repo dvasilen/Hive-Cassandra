dc13469a3985b873520b1294ffefea04d084c3f4
beobal <sam@beobal.com>
2012-09-07 08:31:45 +0100

    DSP-26 Remove base64 encoding from C* Binary values

--- cassandra-handler/src/java/org/apache/hadoop/hive/serde2/lazy/CassandraLazyBinary.java
+++ cassandra-handler/src/java/org/apache/hadoop/hive/serde2/lazy/CassandraLazyBinary.java
@@ -1,14 +1,9 @@
 package org.apache.hadoop.hive.serde2.lazy;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.serde2.lazy.objectinspector.primitive.LazyBinaryObjectInspector;
 import org.apache.hadoop.io.BytesWritable;
 
 public class CassandraLazyBinary extends LazyBinary {
-
-  private static final Log LOG = LogFactory.getLog(LazyBinary.class);
 
   public CassandraLazyBinary(LazyBinaryObjectInspector oi) {
     super(oi);
@@ -24,29 +19,8 @@ public class CassandraLazyBinary extends LazyBinary {
 
   @Override
   public void init(ByteArrayRef bytes, int start, int length) {
-
     byte[] recv = new byte[length];
     System.arraycopy(bytes.getData(), start, recv, 0, length);
-
-    boolean arrayByteBase64 = false;
-    boolean allValid = true;
-
-    for(int i=0; i<recv.length; i++) {
-      if(recv[i] < 0) {
-        allValid = false;
-      }
-    }
-
-    // TODO - need to test this with some base64 data
-    if(allValid) {
-//      arrayByteBase64 = Base64.isArrayByteBase64(recv);
-    }
-
-    if (arrayByteBase64) {
-      LOG.debug("Data not contains valid characters within the Base64 alphabet so " +
-                "decoded the data.");
-    }
-    byte[] decoded = arrayByteBase64 ? Base64.decodeBase64(recv) : recv;
-    data.set(decoded, 0, decoded.length);
+    data.set(recv, 0, recv.length);
   }
 }
